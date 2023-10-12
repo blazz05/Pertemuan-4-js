aabb899d6fb21ec8a1e64bfb486ebefb13f4fee8
Aldi Budiansah <129393970+blazz05@users.noreply.github.com>
2023-10-12 13:03:55 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+// Teks1, mengubah teks string menjadi uppercase dan lowercase
+let bendera = "Merah Putih"
+let uppercase = bendera.toUpperCase()
+
+// Output menampilkan huruf besar
+alert(uppercase) 
+
+// Teks2, mengubah teks string menjafi  lowercase
+let bendera1 = "Merah Putih"
+let lowercase = bendera1.toLowerCase()
+
+// Output menampilkan huruf kecil
+alert(lowercase)
+
+//Teks3, Mengubah teks dalam string
+let bendera3 = "Merah Putih"
+let ubahTeks = bendera3.replace("Putih", "Muda")
+
+// Output untuk menampilakan mengubah teks "putih" menjadi "muda"
+alert(ubahTeks)
+
+// Teks4, untuk menghitung panjang dari variabel Teks
+let bendera4 = "Merah Putih"
+let output = bendera4.length
+
+// Output untuk menampilkan Panjang teks Merah putih
+alert(output)
